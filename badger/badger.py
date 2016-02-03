f7eb2f862ad739ce371d163472530bf1f001e451
doomsuckle <nickkypreos@gmail.com>
2016-02-03 14:52:54 -0800

    set volume to 5

--- badger/badger.py
+++ badger/badger.py
@@ -2,8 +2,8 @@ import subprocess
 import shlex
 
 def badger():
-#    print 'setting volume'
-#    ret = subprocess.Popen(shlex.split('sudo osascript -e "set Volume 5"'), stdout=subprocess.PIPE)
+    print 'setting volume'
+    ret = subprocess.Popen(shlex.split('osascript -e "set Volume 5"'), stdout=subprocess.PIPE)
     print 'talky talky'
     statement = 'say "ho-tel, mo-tel, holiday -- in"'
     ret = subprocess.Popen(shlex.split(statement), stdout=subprocess.PIPE)
