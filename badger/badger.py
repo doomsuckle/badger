bf9e091d7e0ad02a1dfc6c7dbf84477bbfcfae36
doomsuckle <nickkypreos@gmail.com>
2018-11-29 11:40:37 -0800

    update badger.py
    make python3

--- badger/badger.py
+++ badger/badger.py
@@ -2,9 +2,9 @@ import subprocess
 import shlex
 
 def badger():
-    print 'setting volume'
+    print('setting volume')
     ret = subprocess.Popen(shlex.split('osascript -e "set Volume 5"'), stdout=subprocess.PIPE)
-    print 'talky talky'
+    print('talky talky')
     statement = 'say "ho-tel, mo-tel, holiday -- in"'
     ret = subprocess.Popen(shlex.split(statement), stdout=subprocess.PIPE)
 
